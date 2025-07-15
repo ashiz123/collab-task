0e90ff17bf8d63762366fb2f260c7efc00353a49
ashiz123 <37142670+ashiz123@users.noreply.github.com>
2025-07-15 10:40:05 +0100

    added permission middleware

---
+++ App/Services/PermissionRegisteryService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use utils\Logger;
+
+class PermissionRegisteryService {
+
+    public static function all() : array {
+           return [
+           'admin' => ['accept_user', 'delete_user', 'assign_user'] , //manage user
+           'manager' => ['create_task', 'assign_task', 'delete_task', 'set_task_complition', 'task_review'], //manage task
+           'employee' => ['view_task', 'set_task_complition']
+        ];
+    }   
+
+
+    public static function isValid(string $permission): bool{
+       foreach(self::all() as $permissions){
+            if(in_array($permission, $permissions)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static function getPermissionForRole(string $role){
+       $permissions = self::all();
+       return $permissions[$role] ?? [];
+     }
+
+
+    public static function doRoleHasPermission(string $role, string $permission): bool{
+        $permissions = self::getPermissionForRole($role); //get all the permissions by role 
+        Logger::info(json_encode($permissions) . $permission);
+        $hasPermission =  in_array($permission, $permissions); //check the pass permission exist in array in above permissions by role.
+        return $hasPermission;
+    }
+
+}
+
+
+// $getPermission = PermissionRegisteryService::getPermissionForRole('Admin');
+// var_dump($getPermission);
+
+
+
+
+
---
+++ App/middlewares/PermissionMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Middlewares;
+
+
+use App\Services\PermissionRegisteryService;
+
+class PermissionMiddleware{
+
+    private $authService;
+    private $permission;
+
+
+    public function __construct($authService)
+    {
+        $this->authService = $authService;
+        $this->permission = new PermissionRegisteryService();
+    }
+
+    public function handle(callable $next, $permission){
+       
+        $role_title = $this->authService->getAuthUserRole()->title;
+        if(!$this->authService->getAuthenticateUser() || !$this->permission->doRoleHasPermission($role_title, $permission)){
+            http_response_code(403);
+            echo "Forbidden : Permission denied";
+            return;
+        }
+
+        return $next();
+     }
+
+}
+
+ 
